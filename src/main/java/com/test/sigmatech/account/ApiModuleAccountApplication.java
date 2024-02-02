abd43fe905bc06800c8c0e551f436e5b69d5c11b
Syahrul Ilham Muttaqien <syahrulilham62@gmail.com>
2024-02-02 18:29:12 +0700

    initial project

---
+++ src/main/java/com/test/sigmatech/account/ApiModuleAccountApplication.java
@@ -0,0 +1,13 @@
+package com.test.sigmatech.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiModuleAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiModuleAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/sigmatech/account/ApiModuleAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.sigmatech.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiModuleAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
